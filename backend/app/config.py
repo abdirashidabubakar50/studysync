2e5320138ea58851d2f98253cf4fe0d1ef0a28a1
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-12-15 23:14:24 +0300

    🔐 Modified the configuration file
    Modified the configuration file

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,11 +3,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class config:
-    MONGDB_SETTINGS = {
-        'db': os.getenv('DB_NAME', 'studysync'),
-        'host': os.getenv('DB_HOST', 'localhost'),
-        'port': int(os.getenv('DB_PORT', 27017))
-    }
+class Config:
+    # MONGDB_SETTINGS = {
+    #     'db': os.getenv('DB_NAME', 'studysync'),
+    #     'host': os.getenv('DB_HOST', 'localhost'),
+    #     'port': int(os.getenv('DB_PORT', 27017))
+    # }
+    DB_ALIAS = os.getenv('DB_ALIAS', 'core')
+    DB_NAME = os.getenv('DB_NAME', 'studysync')
+    DB_HOST = os.getenv('DB_HOST', 'localhost')
+    DB_PORT = os.getenv('DB_PORT', '27017')
+    DB_USERNAME = os.getenv('DB_USERNAME')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
 
     SECRET_KEY = os.getenv('SECRET_KEY')
+
+    #static method
+    def get_mongo_uri():
+        if Config.DB_USERNAME and Config.DB_PASSWORD:
+            return f"mongodb://{Config.DB_USERNAME}:{Config.DB_PASSWORD}@{Config.DB_HOST}:{Config.DB_PORT}/{Config.DB_NAME}"
+        return f"mongodb://{Config.DB_HOST}:{Config.DB_PORT}/{Config.DB_NAME}"
