9be190b6d664d6022067ff243a92b2b15c81aff3
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-12-15 22:45:30 +0300

    ✨ Added register, login and logout endpoints
    Added register, login and logout endpoints

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -1,8 +1,71 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, session, make_response
 from app.models.user import User
+from mongoengine.errors import NotUniqueError
+from app.utils.jwt_helper import generate_jwt
+from app.utils.token_validation import token_required
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/test', methods=['POST'])
-def test_db():
-    return jsonify({"message": "Auth is working"})
+@auth.route('/register', methods=['POST', 'GET'])
+def register_user():
+    try:
+        # get the data from the request body
+        data = request.get_json()
+
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+
+
+        if not username or not email or not password:
+            return jsonify({'message': 'All fileds (username, email and password) are required'})
+
+        user = User(username=username, email=email, password_hash=User.hash_password(password))
+        user.save()
+
+        return jsonify({'message': 'User registered successfully!'}), 201
+    except NotUniqueError:
+        return jsonify({'message': 'Username or email already exists'}), 409
+    except Exception as e:
+        return jsonify({'message': str(e)}), 500
+
+
+@auth.route('/login', methods=['POST'])
+def login():
+    try:
+        data = request.get_json()
+
+        email = data.get('email')
+        password = data.get('password')
+
+        if not email or not password:
+            return jsonify({'message': 'Email and Password are required'}), 400
+        
+        user = User.objects(email=email).first()
+        if not user or not user.verify_password(password):
+            return jsonify({'message': 'invalid Email or Password'}), 401
+        
+        # generate JWT
+        token = generate_jwt(str(user.id))
+        
+        return jsonify({
+            'message': 'Login successful',
+            'token': token,
+            'user': {'id': str(user.id), 'username': user.username}
+        }); 200
+    except Exception as e:
+        return jsonify({'message': str(e)}), 500
+
+token_blacklist = set()
+
+@auth.route('/logout', methods=['POST'])
+@token_required
+def logout(decoded_payload):
+    try:
+        token = request.headers.get('Authorization').split(" ")[1]
+
+        token_blacklist.add(token)
+
+        return jsonify({'message': 'Logout successful'}), 200
+    except Exception as e:
+        return jsonify({'message': str(e)}), 500
