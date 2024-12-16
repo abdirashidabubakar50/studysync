8f0a7bf00c99f86fbc78c045f713850acc7aea3f
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-12-16 20:45:59 +0300

    🎨 Improved JWT token validation
    Improved JWT token validation

--- backend/app/utils/token_validation.py
+++ backend/app/utils/token_validation.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from app.utils.jwt_helper import decode_jwt
 from flask import request, jsonify
+from app.models.user import User
 
 
 token_blacklist = set()
@@ -10,8 +11,9 @@ def token_required(func):
     @wraps(func)
     def decorated_function(*args, **kwargs):
         token = request.headers.get('Authorization')
+        print("Authorization Header:", token)
         if not token:
-            return jsonify({'message': 'Tokein is missing'}), 401
+            return jsonify({'message': 'Token is missing'}), 401
         
         try:
             token = token.split(" ")[1]
@@ -19,11 +21,19 @@ def token_required(func):
             return jsonify({'message': 'Invalid token format'}), 401
 
         if token in token_blacklist:
-            return jsonify({'meesage': 'invalid token'})
+            return jsonify({'message': 'invalid token'}), 401
         
         try:
             decoded_payload = decode_jwt(token)
+            user_id = decoded_payload.get('user_id')
+            if not user_id:
+                return jsonify({'message': 'Token payload missing user_id'}), 401
+
+            user = User.objects(id=user_id).first()
+            if not user:
+                return jsonify({'message': 'User not found'}), 404
         except Exception as e:
+            print(f"Token decoding error: {e}")
             return jsonify({'message': 'Token is invalid or expired'}), 401
         
         return func(decoded_payload, *args, **kwargs)
