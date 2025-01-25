f1b73c7f14500febaaae0200d9a66acabdea25a5
ABDIRASHID <abdirashidabubakar7@gmail.com>
2025-01-25 12:37:39 +0300

    🔧 Updated configuration file
    Updated configuration file

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,14 @@
+from gevent import monkey
+monkey.patch_all()
+from gevent.pywsgi import WSGIServer
+from app.utils.check_due_assignments import start_scheduler, check_due_assignments
 from app import create_app
+
 
 app = create_app()
 
 if __name__ == '__main__':
+    with app.app_context(): 
+        print("Starting the Flask app...")  
+        start_scheduler()   # Start scheduler
     app.run(debug=True)
