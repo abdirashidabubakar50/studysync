2c273643e4c995174f2895ead58424d3f62f09fb
ABDIRASHID <abdirashidabubakar7@gmail.com>
2025-01-25 12:30:56 +0300

    ✨ Introduced and implemented a notifications featuer
    Introduced and implemented a notifications feature

--- backend/app/models/notifications.py
+++ backend/app/models/notifications.py
@@ -4,8 +4,13 @@ from app.models.module import Module
 from app.models.user import User
 
 class Notification(db.Document):
-    user = ReferenceField(User, required=True)
-    message = StringField(required=True)
-    read = BooleanField(default=False)
-    created_at = DateTimeField(default=datetime.utcnow)
-    type =  StringField()
+    user = db.ReferenceField(User, required=True, reverse_delete_rule=db.CASCADE)
+    message = db.StringField(required=True, max_length=500)
+    read = db.BooleanField(default=False)
+    created_at = db.DateTimeField(default=datetime.datetime.utcnow)
+    type =  db.StringField(choices=["info", "warning", "error", "success"], max_length=50)
+
+    meta = {
+        'db_alias': 'core',
+        'collection': 'notifications'
+    }
